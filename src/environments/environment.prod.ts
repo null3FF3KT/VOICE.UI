95f43785e73d73ebb22b84f2c45510b8666161ba
null3FF3KT <cbilledeaux@gmail.com>
2024-06-30 10:01:32 -0500

    cleaning up build process

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ declare const window: Window & { ENV_VARIABLES?: { [key: string]: string } };
 
 export const environment = {
   production: true, // Set to true in environment.prod.ts
-  functionKey: typeof window !== 'undefined' ? window.ENV_VARIABLES?.FUNCTION_KEY || 'default-dev-key' : 'default-dev-key'
+  functionKey: ''
 };
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,32 @@
-// src/main.ts
-import 'dotenv/config'; 
+import { APP_INITIALIZER, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app-routing.module';
-import { importProvidersFrom } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { environment } from './environments/environment';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { firstValueFrom } from 'rxjs';
+
+function initializeApp(http: HttpClient) {
+  return () => new Promise<void>(async (resolve) => {
+    if (environment.production) {
+      try {
+        const response: any = await firstValueFrom(http.get('/assets/config.json'));
+        environment.functionKey = response.functionKey;
+      } catch (error) {
+        console.error('Failed to load configuration');
+      }
+    }
+    resolve();
+  });
+}
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes),
-    importProvidersFrom(HttpClientModule)
+    importProvidersFrom(HttpClientModule),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: (http: HttpClient) => initializeApp(http),
+      deps: [HttpClient],
+      multi: true
+    }
   ]
-}).catch(err => console.error(err));
+}).catch(err => console.error('Application failed to start'));
